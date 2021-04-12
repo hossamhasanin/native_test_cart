1acb20297f508fae4f4e2675b3f8e49b0bede04f
hossamhasanin <kokojake13@gmail.com>
2021-04-12 22:45:36 +0200

    applied deep link feature using firebase dynamic links

--- app/src/main/java/com/hossam/hasanin/test_cart/deep_link_product/DeeoLinkActivity.java
+++ app/src/main/java/com/hossam/hasanin/test_cart/deep_link_product/DeeoLinkActivity.java
@@ -1,8 +1,10 @@
 package com.hossam.hasanin.test_cart.deep_link_product;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Application;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,9 +14,15 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 import com.hossam.hasanin.test_cart.MainApplication;
 import com.hossam.hasanin.test_cart.R;
 import com.hossam.hasanin.test_cart.cart.CartViewModel;
+import com.hossam.hasanin.test_cart.datasources.MainDataSource;
+import com.hossam.hasanin.test_cart.models.Product;
 
 public class DeeoLinkActivity extends AppCompatActivity {
 
@@ -24,6 +32,7 @@ public class DeeoLinkActivity extends AppCompatActivity {
     TextView tvImage;
     ProgressBar barLoading;
     String productQuery;
+    MainApplication application;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,62 +45,84 @@ public class DeeoLinkActivity extends AppCompatActivity {
         tvImage = findViewById(R.id.tv_image);
 
         viewModel = new ViewModelProvider(this).get(DeepLinkViewModel.class);
-        MainApplication application = (MainApplication) getApplication();
+        application = (MainApplication) getApplication();
         viewModel.previousConnectionStatus = application.isConnected.getValue();
 
 
-        Intent appLinkIntent = getIntent();
-        String appLinkAction = appLinkIntent.getAction();
-        Uri appLinkData = appLinkIntent.getData();
-        if (appLinkData != null){
-            productQuery = appLinkData.getLastPathSegment();
-
-            viewModel.product.observe(this , product -> {
-                if (product != null){
-                    if (product.getProductId() != null) {
-                        barLoading.setVisibility(View.GONE);
-                        tvMess.setVisibility(View.GONE);
-
-                        // those fields are virtual
-                        tvTitle.setVisibility(View.VISIBLE);
-                        tvImage.setVisibility(View.VISIBLE);
-                        tvTitle.setText(product.getProductName());
-                        tvImage.setText(product.getProductImage());
-                    }
-                } else {
-                    tvImage.setVisibility(View.GONE);
-                    tvTitle.setVisibility(View.GONE);
-                    barLoading.setVisibility(View.GONE);
-                    tvMess.setVisibility(View.VISIBLE);
-
-                    tvMess.setText("No product found");
+        viewModel.product.observe(this , product -> {
+            if (product != null){
+                if (product.getProductId() != null) {
+                    showProductData(product);
                 }
-            });
-
-            if (application.isConnected.getValue()){
-                viewModel.getProduct(productQuery);
             } else {
-                tvImage.setVisibility(View.GONE);
-                tvTitle.setVisibility(View.GONE);
-                barLoading.setVisibility(View.GONE);
-                tvMess.setVisibility(View.VISIBLE);
-
-                tvMess.setText("No internet connection");
+                showErrMessage("No product found");
             }
-        }
+        });
+
+        getProduct();
 
         application.isConnected.observe(this, isConnected -> {
             if (isConnected){
                 if (!viewModel.previousConnectionStatus) {
                     Toast.makeText(getBaseContext(), "Internet connected ", Toast.LENGTH_SHORT).show();
                     viewModel.previousConnectionStatus = true;
-                    viewModel.getProduct(productQuery);
+                    getProduct();
                 }
             } else {
                 Toast.makeText(getBaseContext() , "Internet lost " , Toast.LENGTH_SHORT).show();
                 viewModel.previousConnectionStatus = false;
             }
         });
+
+    }
+
+    private void showProductData(Product product){
+        barLoading.setVisibility(View.GONE);
+        tvMess.setVisibility(View.GONE);
+
+        // those fields are virtual
+        tvTitle.setVisibility(View.VISIBLE);
+        tvImage.setVisibility(View.VISIBLE);
+        tvTitle.setText(product.getProductName());
+        tvImage.setText(product.getProductImage());
+    }
+
+    private void showErrMessage(String message){
+        tvImage.setVisibility(View.GONE);
+        tvTitle.setVisibility(View.GONE);
+        barLoading.setVisibility(View.GONE);
+        tvMess.setVisibility(View.VISIBLE);
+
+        tvMess.setText(message);
+    }
+
+    private void getProduct(){
+        Log.v("koko" , getIntent().getData().toString());
+
+        if (application.isConnected.getValue()){
+            FirebaseDynamicLinks.getInstance()
+                    .getDynamicLink(getIntent().getData())
+                    .addOnSuccessListener(this, pendingDynamicLinkData -> {
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+
+                            productQuery = deepLink.getLastPathSegment();
+
+                            viewModel.getProduct(productQuery);
+
+
+                        } else {
+                            // when no link found
+
+                            showErrMessage("Not valid link");
+                        }
+
+                    });
+        } else {
+            showErrMessage("No internet connection");
+        }
+
 
     }
 }
